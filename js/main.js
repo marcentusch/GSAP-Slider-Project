0939277cd0e531a11796d0448933a3a4a8421650
marcentusch <marcentusch@hotmail.com>
2020-08-18 21:57:33 +0200

    add init animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-function init(){
+function init() {
+  // projects element is hidden by default, so we avoid the mess when waiting for gsap to load
+  gsap.set(".projects", { autoAlpha: 1 });
+  gsap.set(".project", { x: "-100%" });
 
-    // welcome to the projects gallery
+  const element = document.querySelector("div.project01");
 
+  tlIn = gsap.timeline();
+  tlIn.fromTo(
+    element,
+    { autoAlpha: 0, x: "-100%" },
+    { duration: 0.7, autoAlpha: 1, x: 0 }
+  );
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+  init();
 });
